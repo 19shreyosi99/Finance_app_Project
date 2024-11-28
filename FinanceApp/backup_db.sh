b3d09012cff1d04e0ad8e78137f2c26b7c291cba
19shreyosi99 <19shreyosi99@gmail.com>
2024-11-28 07:50:24 -0500

    Delete FinanceApp directory

--- FinanceApp/backup_db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DATE=$(date +%F)
-mysqldump -u root -p'admin123' dbo users > ./backup_images/users_backup_$DATE.sql
-mysqldump -u root -p'admin123' dbo budgets > ./backup_images/budgets_backup_$DATE.sql
-mysqldump -u root -p'admin123' dbo transactions > ./backup_images/transactions_backup_$DATE.sql
